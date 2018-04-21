4d8be95fd2f573bd0388ad37b41cb93f78066c10
Ricardo Rivera <rick_rivera662@yahoo.com>
2018-04-20 19:06:03 -0700

    Add README file

---
+++ catalog/README.md
@@ -0,0 +1,48 @@
+# item-catalog-project
+This application is a `Catalog Application` that allows users to catalog items based on categories. The application demonstrates: `API endpoints`, `CRUD` operations on the database via `SQLAlchemy`, local authentication/authorization via `ItsDangerous` and `PassLib`, and third party authentication/authorization via `Facebook's Graph API`. Additionally, `Bootstrap` is used for the styling of the application. 
+
+For testing purposes the `catalog` database is populated with `video game` data. This data consists of `video game categories` as the `categories` and `video games` as the `items` themselves.  
+
+## Getting Started
+These instructions will get you a copy of the project on your local machine for development and/or testing purposes.
+
+### Prerequisites
+To use the `VirtualBox` virtual machine (VM) the user will require [VirtualBox 5.1.34](https://www.virtualbox.org/wiki/Downloads)
+or higher and [Vagrant 1.9.2](https://www.vagrantup.com/downloads.html) or higher . 
+
+To run the python script the user will require [Python 2.7.14](https://www.python.org/downloads/) or higher.
+
+To view the webpage correctly an internet connection is required. 
+
+### Installing
+To get a copy of the project to work on locally, the user can either `download the zip` or `clone the repository`.
+
+### Initial Setup
+In order for the python script to run correctly, the `catalog` database must be created. To create the database:
+1) Open Terminal/Powershell.
+2) Using the `cd` command, navigate to the directory where the project is located.
+3) Start the VM by using the `vagrant up` command.
+4) Connect to the VM using the `vagrant ssh` command.
+5) Navigate to the directory in the VM where the `games.py` script is located using the command `cd`.
+6) Create the database using the command `python games.py`
+
+## Run the Project
+In order to run the project:
+1) Open Terminal/Powershell.
+2) Using the `cd` command, navigate to the directory where the project is located.
+3) Start the VM by using the command `vagrant up`.
+4) Connect to the VM using the command `vagrant ssh`.
+5) Navigate to the directory in the VM where the python script is located using the command `cd`.
+6) Run the python script using the command `python application.py`.
+7) Using a web browser navigate to the catalog web page at `http://localhost:5000/catalog/`. 
+
+## Built with
+* `Python 2.7.14`
+* `Flask 0.9`
+* `SQLAclchemy 1.2.5`
+* `ItsDangerous 0.20`
+* `PassLib 1.7.1`
+* `Bootstrap 4.1.0`
+
+## Authors
+* Ricardo Rivera
--- catalog/games.py
+++ catalog/games.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_model import Base, User, Category, Item
 import datetime
+from passlib.apps import custom_app_context as pwd_context
+
 
 # Setup database. 
 engine = create_engine("sqlite:///catalog.db")
@@ -9,9 +12,13 @@ Base.metadata.bind = engine
 dbSession = sessionmaker(bind=engine)
 session = dbSession()
 
-# Create user.
-user = User(name="Juan Camaney", email="rr@email.com")
-session.add (user)
+# Create users.
+user1 = User(name="luffy123", email="luffy123@email.com", password_hash=pwd_context.encrypt("!@#$qwer"))
+session.add (user1)
+session.commit()
+
+user2 = User(name="chopper123", email="chopper123@email.com", password_hash=pwd_context.encrypt("!@#$qwer"))
+session.add (user2)
 session.commit()
 
 # Create categories.
@@ -27,32 +34,76 @@ category3 = Category(category="Sports", user_id=1)
 session.add(category3)
 session.commit()
 
+category4 = Category(category="Role-Playing", user_id=2)
+session.add(category4)
+session.commit()
+
 # Create items.
 item1 = Item(name="God of War", description="It is a new beginning for Kratos."
-"Living as a man outside the shadow of the gods, he ventures into the brutal"
-"Norse wilds with his son Atreus, fighting to fulfill a deeply personal quest.",
+" Living as a man outside the shadow of the gods, he ventures into the brutal"
+" Norse wilds with his son Atreus, fighting to fulfill a deeply personal quest.",
 category_id=1, user_id=1)
 session.add(item1)
 session.commit()
 
 item2 = Item(name="Red Dead Redemption 2", description="Developed by the creators of"
-"Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale"
-"of life in America's unforgiving heartland. ",
+" Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale"
+" of life in America's unforgiving heartland. ",
 category_id=1, user_id=1)
 session.add(item2)
 session.commit()
 
 item3 = Item(name="NBA2K18", description="The highest rated* annual sports title"
-"of this console generation returns with NBA 2K18, featuring unparalleled authenticity"
-"and improvements on the court. *According to 2008 - 2016 Metacritic.com",
+" of this console generation returns with NBA 2K18, featuring unparalleled authenticity"
+" and improvements on the court. *According to 2008 - 2016 Metacritic.com",
 category_id=3, user_id=1)
 session.add(item3)
 session.commit()
 
 item4 = Item(name="Mortal Kombat XL", description="One of the best-selling titles of 2015"
-"has gone XL! Komplete The Mortal Kombat X Experience with new and existing content",
+" has gone XL! Komplete The Mortal Kombat X Experience with new and existing content",
 category_id=2, user_id=1)
 session.add(item4)
 session.commit()
+
+item5 = Item(name="Yakuza 6", description="In Yakuza 6, Kazuma Kiryu will find out exactly"
+" how much people are willing to sacrifice for family - be those ties through blood or bond"
+" - when he investigates a series of shadowy events that involve the ones he holds closest"
+" to his heart.",
+category_id=1, user_id=2)
+session.add(item5)
+session.commit()
+
+item6 = Item(name="MLB The Show 18", description="Your team. Your rivals. Your buddies."
+" Your hometown. No matter why you play or why you love baseball, MLB The Show 18"
+" is for a fan like you.", category_id=3, user_id=2)
+session.add(item6)
+session.commit()
+
+item7 = Item(name="Kingdom Hearts 3", description="", category_id=4, user_id=2)
+session.add(item7)
+session.commit()
+
+item8 = Item(name="Madden NFL 19", description="", category_id=3, user_id=1)
+session.add(item8)
+session.commit()
+
+item9 = Item(name="Nights of Azure 2", description="The story unfolds in a fictional,"
+" demon-ridden Western European city towards the end of the 19th century and follows"
+" Alushe, a knight protector who was ambushed and killed while guarding her childhood"
+" friend and Curia Priestess, Liliana.",
+category_id=4, user_id=2)
+session.add(item9)
+session.commit()
+
+item10 = Item(name="Tales of Berseria", description="In TALES OF BERSERIA, players"
+" embark on a journey of self-discovery as they assume the role of Velvet, a young"
+" woman whose once kind demeanor has been replaced and overcome with a festering anger"
+" and hatred after a traumatic experience three years prior to the events within TALES"
+" OF BERSERIA.",
+category_id=4, user_id=1)
+session.add(item10)
+session.commit()
+
 
 print("Added items.")
